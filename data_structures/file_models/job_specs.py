798cef1c2b8748ecb33e52c7b2c5a7a2899738c3
Pseudolukian <pynanist@gmail.com>
2023-08-01 13:27:41 +0000

    Alpha_09

--- data_structures/file_models/job_specs.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class job_specs(BaseModel):
-    specialty:str = None
-    position:str = None
-    responsibilities:str = None
--- data_structures/file_models/sql_buffer.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-from data_structures.file_models import OutputModel
-
-class SQL_buffer(BaseModel):
-    buf: List[List[OutputModel]] = []
-    
-    def add(self, *data: OutputModel):
-        self.buf.append(list(data))
